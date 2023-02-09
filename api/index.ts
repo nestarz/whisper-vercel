3030bdc96c1613127ced45772a1933d6c36ffdc9
Elias Rhouzlane <elias.rhouzlane@protonmail.com>
2023-02-09 19:33:51 +0100

    fix: path dl

--- api/index.ts
+++ api/index.ts
@@ -4,23 +4,16 @@ import vocab from "../assets/vocab_en.json" assert { type: "json" };
 import melFilters from "../assets/mel_filters.json" assert { type: "json" };
 
 console.log("fetching...");
-const a = await fetch(
+await fetch(
   new URL(
     "https://pub-10e35d3e9dcf488ebc5a30272db639a4.r2.dev/silero_vad.with_runtime_opt.ort"
   ).href
 )
   .then((r) => r.arrayBuffer())
-  .then((r) =>
-    Deno.writeFile(
-      new URL("./silero_vad.with_runtime_opt.ort", import.meta.url).href,
-      r
-    )
-  );
-
-console.log(a.byteLength);
+  .then((r) => Deno.writeFile("./silero_vad.with_runtime_opt.ort", r));
 
 const full = await ort.InferenceSession.create(
-  new URL("./silero_vad.with_runtime_opt.ort", import.meta.url).href
+  new URL("../silero_vad.with_runtime_opt.ort", import.meta.url).href
 );
 
 console.log(full);
