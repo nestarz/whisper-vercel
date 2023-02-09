f70e43f0cc316520f4fbe680da17ffbc71c6b0c1
Elias Rhouzlane <elias.rhouzlane@protonmail.com>
2023-02-09 21:07:40 +0100

    fix: whisper

--- api/index.ts
+++ api/index.ts
@@ -3,23 +3,13 @@ import { logMelSpectogram, decode, trimOrPad, resample } from "./utils.ts";
 import vocab from "../assets/vocab_en.json" assert { type: "json" };
 import melFilters from "../assets/mel_filters.json" assert { type: "json" };
 
-const t0 = Date.now();
-if (
-  await Deno.writeFile("./test", new Uint8Array([0, 0]))
-    .then(() => false)
-    .catch((e) => console.log(e) ?? true)
-)
-  setInterval(() => console.log("load", (Date.now() - t0) / 1000), 100);
-const full = await ort.InferenceSession.create(
-  new URL(
-    "https://pub-10e35d3e9dcf488ebc5a30272db639a4.r2.dev/whisper_tiny_en_20_tokens.ort"
-  ).href
-);
-console.log("loaded", (Date.now() - t0) / 1000);
+const modelPath =
+  "https://pub-10e35d3e9dcf488ebc5a30272db639a4.r2.dev/whisper_tiny_en_20_tokens.ort";
 
-console.log(full);
-
+let model;
 const createWhisper = async ({ sampleRate = 8000 }) => {
+  model = model ?? (await ort.InferenceSession.create(modelPath));
+
   return async (bytes: Uint8Array) => {
     const pcm = Float32Array.from(
       resample(new Int16Array(bytes.buffer), sampleRate, 16000),
@@ -36,4 +26,12 @@ const createWhisper = async ({ sampleRate = 8000 }) => {
   };
 };
 
-export default () => new Response(`Hello, from Deno v${Deno.version.deno}!`);
+export default async (req: Request) => {
+  if (req.method !== "POST") return new Response(null);
+
+  const { sample_rate } = Object.fromEntries(new URLSearchParams(req.url));
+  const whisper = await createWhisper({ sampleRate: Number(sample_rate) });
+  const result = await whisper(new Uint8Array(await req.arrayBuffer()));
+
+  return new Response(result, { headers: { "content-type": "text/plain" } });
+};
